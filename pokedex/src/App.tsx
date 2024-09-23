a5f3e25a607ab7a8508a5ee1e6efa2b63097af58
Harrison Craythorne <hcr49@uclive.ac.nz>
2024-09-23 16:46:56 +1200

    Temp test code and route for viewing card objects

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";import './App.css';
 import NotFound from "./components/NotFound";
+import PokemonCardObject from "./components/PokemonCardObject";
+
+import { Pokemon, PokemonClient } from 'pokenode-ts';
 
 function App() {
+
+    const [pokemon, setPokemon] = React.useState<Pokemon>();
+    const api = new PokemonClient();
+
+    React.useEffect(() => {
+        const getPokemon = () => {
+            api.getPokemonById(1)
+                .then((response) => {setPokemon(response)})
+                .catch((error) => {console.log(error);});
+        }
+        getPokemon();
+    })
+
+
+
   return (
       <div className="App">
         <Router>
           <div>
             <Routes>
-              <Route path="*" element={<NotFound/>}/>
+                <Route path="/pokemon" element={<PokemonCardObject pokemon={pokemon}/>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
           </div>
         </Router>
