8f4177ab63759849468e9e457138ab60f90bdaf1
Harrison Craythorne <hcr49@uclive.ac.nz>
2024-09-25 12:47:20 +1200

    Added path for pokemon detailed view

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './App.css';
 import NotFound from "./components/NotFound";
-import PokemonCardObject from "./components/PokemonCardObject";
-
-import { Pokemon, PokemonClient } from 'pokenode-ts';
 import PokemonGrid from "./components/PokemonGrid";
+import PokemonDetailedView from "./components/PokemonDetailedView";
 
 function App() {
   return (
@@ -14,6 +13,7 @@ function App() {
           <div>
             <Routes>
                 <Route path="/pokemon" element={<PokemonGrid/>}/>
+                <Route path="/pokemon/:pokemonId" element={<PokemonDetailedView/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
           </div>
