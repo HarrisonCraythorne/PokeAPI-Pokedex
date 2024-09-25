1d48e0473aef3adad8d94b27df2560f4e861db9c
Harrison Craythorne <hcr49@uclive.ac.nz>
2024-09-25 13:32:07 +1200

    Updated default colour and hover colour to match pokemon theme

--- pokedex/src/components/BackButton.tsx
+++ pokedex/src/components/BackButton.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import {Button} from "@mui/material";
+import {darken} from '@mui/system';
 
-interface BackButtonProps {
+interface IBackButtonProps {
     text?: string
     fullWidth?: boolean;
+    bgColour?: string;
+    textColour?: string;
 }
 
-const BackButton: React.FC<BackButtonProps> = ({text, fullWidth}) => {
+const BackButton = (props: IBackButtonProps) => {
     const navigate = useNavigate();
+    const bgColour = props.bgColour ? props.bgColour : '#ff0000';
+    const textColour = props.textColour ? props.textColour : 'white';
 
     const handleClick = () => {
         navigate(-1);
     };
 
     return (
-        <Button variant="contained" fullWidth={fullWidth} onClick={handleClick}>
-            {text ? text : "Go Back"}
+        <Button variant="contained"
+                fullWidth={props.fullWidth}
+                onClick={handleClick}
+                sx={{bgcolor: bgColour,
+                    color: textColour,
+                    '&:hover': {
+                        backgroundColor: darken(bgColour, 0.2)
+                    }}}
+        >
+            {props.text ? props.text : "Go Back"}
         </Button>
     );
 };
